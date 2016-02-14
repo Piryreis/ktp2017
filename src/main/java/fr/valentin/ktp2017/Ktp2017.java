d6254115c465be0bd479ee4d46d73509a307b3d4
Piryreis <valentin.guillaumot6@orange.fr>
2016-02-14 17:08:24 +0100

    Base du plugin

---
+++ src/main/java/fr/valentin/ktp2017/Ktp2017.java
@@ -0,0 +1,20 @@
+package fr.valentin.ktp2017;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Val'entin.
+ */
+public class Ktp2017 extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+
+}
