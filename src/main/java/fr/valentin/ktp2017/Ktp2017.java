802eef563089c464dea982f60615e96c0b89be01
Piryreis <valentin.guillaumot6@orange.fr>
2016-04-03 19:06:15 +0200

    Mise à jour de la class Main/Principale

--- src/main/java/fr/valentin/ktp2017/Ktp2017.java
+++ src/main/java/fr/valentin/ktp2017/Ktp2017.java
@@ -4,8 +4,8 @@ import fr.valentin.ktp2017.arena.ArenaManager;
 import fr.valentin.ktp2017.command.ktp2017Commands;
 import fr.valentin.ktp2017.config.Config;
 import fr.valentin.ktp2017.game.GameManager;
-import fr.valentin.ktp2017.listener.PlayerJoin;
-import fr.valentin.ktp2017.listener.PlayerLogin;
+import fr.valentin.ktp2017.listener.*;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -30,9 +30,17 @@ public class Ktp2017 extends JavaPlugin {
         instance = this;
         this.pluginManager = this.getServer().getPluginManager();
 
+        // Load Config
+        saveDefaultConfig();
+
         // Events Listeners
         pluginManager.registerEvents(new PlayerJoin(), this);
         pluginManager.registerEvents(new PlayerLogin(), this);
+        pluginManager.registerEvents(new PlayerQuit(), this);
+        pluginManager.registerEvents(new EntityDomage(), this);
+        pluginManager.registerEvents(new PlayerDeath(), this);
+        pluginManager.registerEvents(new PlayerMove(), this);
+        pluginManager.registerEvents(new GameWon(), this);
 
         // Command
         getCommand("ktp2017").setExecutor(new ktp2017Commands());
@@ -40,16 +48,22 @@ public class Ktp2017 extends JavaPlugin {
         // Load
         ArenaManager.loadArena();
         GameManager.loadGame();
+
     }
 
     @Override
     public void onDisable(){
         GameManager.removeGame();
         ArenaManager.removeArena();
+        saveConfig();
     }
 
     public static void log(String message){
         instance.getLogger().info(message);
+    }
+
+    public static void broadcastMessage(String message){
+        Bukkit.getServer().broadcastMessage(getTag() + message);
     }
 
     public static String getTag(){
@@ -58,5 +72,9 @@ public class Ktp2017 extends JavaPlugin {
 
     public Config getConfiguration(){
         return Config.getInstance();
+    }
+
+    public PluginManager getPluginManager(){
+        return pluginManager;
     }
 }
